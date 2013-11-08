25fb8a46cfb26ffb660f1907223902bd9d8b9a4f
Miguel Escriva <miesgre@gmail.com>
2013-11-08 14:52:56 +0100

    Captions Data Adapter for Matterhorn implemented

--- paella-matterhorn/javascript/paella_matterhorn.js
+++ paella-matterhorn/javascript/paella_matterhorn.js
@@ -268,6 +268,100 @@ paella.dataDelegates.MHAnnotationServiceTrimmingDataDelegate = Class.create(pael
 });
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Captions Loader
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+paella.matterhorn.DFXPParser = Class.create({
+	
+	parseCaptions:function(text)
+	{
+		var xml = $(text);
+		var ps = xml.find("body div p");
+		var captions= [];
+		var i = 0;		
+		for (i=0; i< ps.length; i++) {		
+			var c = this.getCaptionInfo(ps[i]);
+			c.id = i;
+			captions.push(c);
+		}		
+		return captions;
+	},
+	
+	getCaptionInfo:function(cap) {
+		var b = this.parseTimeTextToSeg(cap.getAttribute("begin"));
+		var d = this.parseTimeTextToSeg(cap.getAttribute("end"));
+		var v = $(cap).text();
+		
+		return {s:b, d:d, e:b+d, name:v, content:v};
+	},
+	
+	parseTimeTextToSeg:function(ttime){
+		var split = ttime.split(":");
+		var h = parseInt(split[0]);
+		var m = parseInt(split[1]);
+		var s = parseInt(split[2]);
+		return s+(m*60)+(h*60*60);
+	},
+	
+	captionsToDxfp:function(captions){
+		var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+		xml = xml + '<tt xml:lang="en" xmlns="http://www.w3.org/2006/10/ttaf1" xmlns:tts="http://www.w3.org/2006/04/ttaf1#styling">\n';
+		xml = xml + '<body><div xml:id="captions" xml:lang="en">\n';
+		
+		for (var i=0; i<captions.length; i=i+1){
+			var c = captions[i];
+			xml = xml + '<p begin="'+ paella.utils.timeParse.secondsToTime(c.begin) +'" end="'+ paella.utils.timeParse.secondsToTime(c.duration) +'">' + c.value + '</p>\n';
+		}
+		xml = xml + '</div></body></tt>';
+		
+		return xml;
+	}
+});
+
+paella.dataDelegates.MHCaptionsDataDelegate = Class.create(paella.DataDelegate,{
+	read:function(context,params,onSuccess) {
+		var catalogs = paella.matterhorn.episode.mediapackage.metadata.catalog;
+		if (!(catalogs instanceof Array)) {
+			catalogs = [catalogs];
+		}
+		
+		for (var i=0; i<catalogs.length; ++i) {
+			var catalog = catalogs[i];
+			
+			if (catalog.type == 'captions/timedtext') {
+				// Load Captions!
+				paella.ajax.get({url: catalog.url},	
+					function(data, contentType, returnCode, dataRaw) {
+					
+					
+						var parser = new paella.matterhorn.DFXPParser();
+						var captions = parser.parseCaptions(data);						
+						if (onSuccess) onSuccess({captions:captions}, true);
+											
+
+					},
+					function(data, contentType, returnCode) {
+						if (onSuccess) { onSuccess({}, false); }
+					}
+				);				
+				
+				return;
+			}
+		}
+		
+		if (onSuccess) { onSuccess({}, false); }
+	},
+	
+	write:function(context,params,value,onSuccess) {
+		if (onSuccess) { onSuccess({}, false); }
+	},
+	
+	remove:function(context,params,onSuccess) {
+		if (onSuccess) { onSuccess({}, false); }
+	}
+});
+
+
 paella.dataDelegates.MHFootPrintsDataDelegate = Class.create(paella.DataDelegate,{
 	read:function(context,params,onSuccess) {
 		var episodeId = params.id;
