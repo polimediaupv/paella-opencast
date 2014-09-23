b0ee6bad4ce7388f93463e489ce6d829c901309c
Miguel Escrivá <miesgre@gmail.com>
2014-09-23 14:15:22 +0200

    Split paella_matterhorn.js into multiple files

---
+++ paella-matterhorn/javascript/03_videoloader.js
@@ -0,0 +1,140 @@
+/*
+
+Video data: paella.VideoLoader
+
+Extend paella.VideoLoader and implement the loadVideo method:
+
+*/
+
+
+var MHVideoLoader = Class.create(paella.VideoLoader, {
+	isStreaming:function(track) {
+		return /rtmp:\/\//.test(track.url);
+	},
+
+	getStreamSource:function(track) {
+		var res = new Array(0,0);
+        if (track.video instanceof Object) {
+		    res = track.video.resolution.split('x');
+        }
+
+		var source = {
+			src:  track.url,
+			type: track.mimetype,
+			res: {w:res[0], h:res[1]},
+			isLiveStream: false
+		};
+
+		return source;
+	},
+
+
+
+	loadVideo:function(videoId,onSuccess) {
+		var i;
+		var streams = {};
+		var tracks = paella.matterhorn.episode.mediapackage.media.track;
+		var attachments = paella.matterhorn.episode.mediapackage.attachments.attachment;
+		if (!(tracks instanceof Array)) { tracks = [tracks]; }
+		if (!(attachments instanceof Array)) { attachments = [attachments]; }
+		this.frameList = {};
+
+
+		// Read the tracks!!
+		for (i=0;i<tracks.length;++i) {
+			var currentTrack = tracks[i];
+			var currentStream = streams[currentTrack.type];
+			if (currentStream == undefined) { currentStream = { sources:{}, preview:'' }; }
+			
+			
+			if (this.isStreaming(currentTrack)) {
+				if ( !(currentStream.sources['rtmp']) || !(currentStream.sources['rtmp'] instanceof Array)){
+					currentStream.sources['rtmp'] = [];
+				}
+				currentStream.sources['rtmp'].push(this.getStreamSource(currentTrack));
+			}
+			else{
+				var videotype = null;
+				switch (currentTrack.mimetype) {
+					case 'video/mp4':
+					case 'video/ogg':
+					case 'video/webm':
+						videotype = currentTrack.mimetype.split("/")[1];
+						break;
+					case 'video/x-flv':
+						videotype = 'flv';
+						break;
+					default:
+						paella.debug.log('MHVideoLoader: MimeType ('+currentTrack.mimetype+') not recognized!');
+						break;
+				}
+				if (videotype){
+					if ( !(currentStream.sources[videotype]) || !(currentStream.sources[videotype] instanceof Array)){
+						currentStream.sources[videotype] = [];
+					}				
+					currentStream.sources[videotype].push(this.getStreamSource(currentTrack));
+				}
+			}
+
+			streams[currentTrack.type] = currentStream;
+		}
+		
+		var presenter = streams["presenter/delivery"];
+		var presentation = streams["presentation/delivery"];		
+		var imageSource =   {type:"image/jpeg", frames:{}, count:0, duration: parseInt(paella.matterhorn.episode.mediapackage.duration/1000), res:{w:320, h:180}};
+		var imageSourceHD = {type:"image/jpeg", frames:{}, count:0, duration: parseInt(paella.matterhorn.episode.mediapackage.duration/1000), res:{w:1280, h:720}};
+		// Read the attachments
+		for (i=0;i<attachments.length;++i) {
+			var currentAttachment = attachments[i];
+
+			if (currentAttachment.type == "presentation/segment+preview+hires") {
+				if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+					time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+					imageSourceHD.frames["frame_"+time] = currentAttachment.url;
+					imageSourceHD.count = imageSourceHD.count +1;
+                	
+                	if (!(this.frameList[time])){
+	                	this.frameList[time] = {id:'frame_'+time, mimetype:currentAttachment.mimetype, time:time, url:currentAttachment.url, thumb:currentAttachment.url};                	
+                	}
+                	this.frameList[time].url = currentAttachment.url;
+				}
+			}
+			else if (currentAttachment.type == "presentation/segment+preview") {
+				if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+					time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+					imageSource.frames["frame_"+time] = currentAttachment.url;
+					imageSource.count = imageSource.count +1;
+					
+                	if (!(this.frameList[time])){
+	                	this.frameList[time] = {id:'frame_'+time, mimetype:currentAttachment.mimetype, time:time, url:currentAttachment.url, thumb:currentAttachment.url};                	
+                	}
+                	this.frameList[time].thumb = currentAttachment.url;
+				}
+			}
+			else if (currentAttachment.type == "presentation/player+preview") {
+				presentation.preview = currentAttachment.url;
+			}
+			else if (currentAttachment.type == "presenter/player+preview") {
+				presenter.preview = currentAttachment.url;
+			}
+		}
+
+		// Set the image stream
+		var imagesArray = [];
+		if (imageSourceHD.count > 0) { imagesArray.push(imageSourceHD); }
+		if (imageSource.count > 0) { imagesArray.push(imageSource); }
+		if ( (imagesArray.length > 0) && (presentation != undefined) ){
+			presentation.sources.image = imagesArray; 
+		}
+		
+		
+	
+		if (presenter) { this.streams.push(presenter); }
+		if (presentation) { this.streams.push(presentation); }
+
+		// Callback
+		this.loadStatus = true;
+		onSuccess();			
+	}
+});
+
---
+++ paella-matterhorn/javascript/05_initdelegate.js
@@ -0,0 +1,149 @@
+
+function initPaellaMatterhornMe(onSuccess, onError) {
+
+	base.ajax.get({url:'/info/me.json'},
+		function(data,contentType,code) {
+			paella.matterhorn.me = data;
+			if (onSuccess) onSuccess();
+		},
+		function(data,contentType,code) { if (onError) onError(); }
+	);
+}
+
+function initPaellaMatterhorn(episodeId, onSuccess, onError) {
+
+	initPaellaMatterhornMe(
+		function(){
+			base.ajax.get({url:'/search/episode.json', params:{'id': episodeId}},
+				function(data,contentType,code) {
+					paella.matterhorn.episode = data['search-results'].result;
+					var asyncLoader = new paella.AsyncLoader();
+		
+					if (paella.matterhorn.episode) {
+						var serie = paella.matterhorn.episode.mediapackage.series;
+						
+						if (serie != undefined) {
+							asyncLoader.addCallback(new paella.JSONCallback({url:'/series/'+serie+'.json'}), "serie");
+							asyncLoader.addCallback(new paella.JSONCallback({url:'/series/'+serie+'/acl.json'}), "acl");
+						
+							asyncLoader.load(function() {
+									//Check for series
+									paella.matterhorn.serie = asyncLoader.getCallback("serie").data;
+									//Check for acl
+									paella.matterhorn.acl = asyncLoader.getCallback("acl").data;
+									if (onSuccess) onSuccess();
+								},
+								function() {
+									if (onError) onError();
+								}
+							);
+						}
+						else {
+							if (onSuccess) onSuccess();						
+						}
+					}
+					else {
+						if (onError) onError();
+					}
+				},
+				function(data,contentType,code) { if (onError) onError(); }
+			);
+		},
+		function() { if (onError) onError(); }
+	);
+}
+
+
+
+paella.matterhorn.InitDelegate = Class.create(paella.InitDelegate,{
+	loadConfig:function(onSuccess) {
+		var configUrl = this.initParams.configUrl;
+		var params = {};
+		params.url = configUrl;
+		paella.ajax.get(params,function(data, type, returnCode, responseRaw) {
+				if (typeof(data)=='string') {
+					try {
+						data = JSON.parse(data);
+					}
+					catch (e) {
+						onSuccess({});
+					}
+				}
+				onSuccess(data);
+			},
+			function(data, type,returnCode, responseRaw) {
+				if (returnCode == 200){
+					if (typeof(responseRaw)=='string') {
+						try {
+							data = JSON.parse(responseRaw);
+						}
+						catch (e) {
+							data = {};
+						}
+					}
+					onSuccess(data);
+				}
+				else{
+					onSuccess({});
+				}
+			});
+	}
+});
+
+function loadPaella(containerId, onSuccess) {
+	var initDelegate = new paella.matterhorn.InitDelegate({accessControl:new MHAccessControl(),videoLoader:new MHVideoLoader()});
+	var id = paella.utils.parameters.get('id');
+
+	initPaellaMatterhorn(id,
+		function() {
+			initPaellaEngage(containerId,initDelegate);
+			if (onSuccess) onSuccess();
+		},
+		function() {
+			if (paella.matterhorn.me.username == "anonymous") {
+				window.location.href = "auth.html?redirect=" + encodeURIComponent(window.location.href);
+			}
+			else {
+				paella.messageBox.showError("Error loading video " + id);
+			}		
+		}
+	);
+}
+
+function loadPaellaExtended(containerId, onSuccess) {
+	var initDelegate = new paella.matterhorn.InitDelegate({accessControl:new MHAccessControl(),videoLoader:new MHVideoLoader()});
+	var id = paella.utils.parameters.get('id');
+
+	initPaellaMatterhorn(id,
+		function() {
+			initPaellaExtended({containerId:containerId,initDelegate:initDelegate});
+			if (onSuccess) onSuccess();
+		},
+		function() {
+			if (paella.matterhorn.me.username == "anonymous") {
+				window.location.href = "auth.html?redirect=" + encodeURIComponent(window.location.href);
+			}
+			else {
+				paella.messageBox.showError("Error loading video " + id);
+			}		
+		}
+	);
+}
+
+function loadPaellaEditor(containerId) {
+    var EditorInitDelegate = Class.create(paella.matterhorn.InitDelegate,{
+            loadConfig:function(onSuccess) {
+				this.parent(function(data) {
+					if (data.editor) {
+						data.editor.loadOnStartup = true;
+					}
+					if (onSuccess) { onSuccess(data); }
+				});
+            }
+    });
+
+	var initDelegate = new EditorInitDelegate({accessControl:new MHAccessControl(),videoLoader:new MHVideoLoader()});
+	var id = paella.utils.parameters.get('id');
+
+	initPaellaMatterhorn(id, function() {initPaellaEngage(containerId,initDelegate);});
+}
