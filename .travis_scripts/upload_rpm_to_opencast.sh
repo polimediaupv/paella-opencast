ce3a76d7487d849a43e34b4bea13b4290b82ad4e
Miguel Escriva <miesgre@gmail.com>
2019-03-05 16:35:09 +0100

    travis

--- .travis_scripts/upload_rpm_to_opencast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export VERSION=$(sed -n '0,/^.*<version>\(.*\)<\/version>.*$/s//\1/p' pom.xml)
-curl -i -u "${PKGUPLOADUSER}:${PKGUPLOADPASS}" \
-     -F file=@${HOME}/rpmbuild/RPMS/noarch/paella-opencast-${VERSION}-${TRAVIS_BUILD_NUMBER}.${TRAVIS_COMMIT}.noarch.rpm \
-     https://pkg.opencast.org/upload
